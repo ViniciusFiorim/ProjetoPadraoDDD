e70d0c7114eb60afd86ae1a285542e6e42a70451
Vinicius Fiorim <vinicius.fiorim@outlook.com>
2023-02-05 13:52:32 -0300

    Aprimorando e criando projeto base baseado em orientação de video aula do Eduardo pires

---
+++ PojetoPadraoDDD/ProjetoPadrao.Infra.Data/Context/ProjetoPadraoContext.cs
@@ -0,0 +1,15 @@
+﻿using System.Data.Entity;
+using ProjetoPadrao.Domain.Entities;
+
+namespace ProjetoPadrao.Infra.Data.Context
+{
+    public class ProjetoPadraoContext : DbContext
+    {
+        public ProjetoPadraoContext() :  base("ProjetoPadraoDDD")
+        {
+            
+        }
+        
+        public DbSet<Cliente>  Clientes { get; set; }
+    }
+}
---
+++ PojetoPadraoDDD/ProjetoPadrao.Web.MVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ProjetoPadrao.Web.MVC.MvcApplication" Language="C#" %>
