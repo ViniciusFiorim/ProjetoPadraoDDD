9e3f8a78a71d38e7311ff86957d2da375ae2f90d
Vinicius Fiorim <vinicius.fiorim@outlook.com>
2023-03-18 16:54:23 -0300

    Ajustes no codigo, index funcional , porem tendo problema com creates

--- Infra.Data/Migrations/Configuration.cs
+++ Infra.Data/Migrations/Configuration.cs
@@ -1,18 +1,23 @@
-﻿using System.Data.Entity.Migrations;
-using Infra.Data.Context;
-
-namespace Infra.Data.Migrations
+﻿namespace Infra.Data.Migrations
 {
-    internal sealed class Configuration : DbMigrationsConfiguration<ProjetoModeloContext>
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<Infra.Data.Context.ProjetoModeloContext>
     {
         public Configuration()
         {
             AutomaticMigrationsEnabled = true;
         }
 
-        protected override void Seed(ProjetoModeloContext context)
+        protected override void Seed(Infra.Data.Context.ProjetoModeloContext context)
         {
-            // base.Seed(context);
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
         }
-    } 
+    }
 }
