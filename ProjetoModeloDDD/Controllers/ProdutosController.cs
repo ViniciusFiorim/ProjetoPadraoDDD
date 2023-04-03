61619c9e0c1c829d0c169420d33aa7642034738d
Vinicius Fiorim <vinicius.fiorim@outlook.com>
2023-04-03 00:32:22 -0300

    Padronizaçãod e metodos de save do produto entre demais operações

--- ProjetoModeloDDD/Controllers/ProdutosController.cs
+++ ProjetoModeloDDD/Controllers/ProdutosController.cs
@@ -19,16 +19,28 @@ namespace ProjetoModeloDDD.Controllers
         [HttpGet]
         public ActionResult Index()
         {
-            var  produtoViewModel = Mapper.Map<IEnumerable<Produto>, IEnumerable<ProdutoViewModel>>(_produtoApp.GetAll());
+            var produtoViewModel = Mapper.Map<IEnumerable<Produto>,
+                IEnumerable<ProdutoViewModel>>(_produtoApp.GetAll());
+
             return View(produtoViewModel);
         }
-        
-        public ActionResult Create() 
+
+        public ActionResult Detalhes(int id)
         {
-            ViewBag.Message = "Novo produto";
+            ViewBag.Message = "Informações do Produto";
+
+            var produto = _produtoApp.GetById(id);
+            var produtoViewModel = Mapper.Map<Produto, ProdutoViewModel>(produto);
+
+            return View(produtoViewModel);
+        }
+
+        public ActionResult Create()
+        {
+            ViewBag.Message = "Novo Produto";
             return View();
         }
-        
+
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult Create(ProdutoViewModel produto)
@@ -42,6 +54,51 @@ namespace ProjetoModeloDDD.Controllers
             }
 
             return View(produto);
+        }
+
+        public ActionResult Editar(int id)
+        {
+            ViewBag.Message = "Editar Produto";
+
+            var produto = _produtoApp.GetById(id);
+            var produtoViewModel = Mapper.Map<Produto, ProdutoViewModel>(produto);
+
+            return View(produtoViewModel);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Editar(ProdutoViewModel produto)
+        {
+            if (ModelState.IsValid)
+            {
+                var produtoDomain = Mapper.Map<ProdutoViewModel, Produto>(produto);
+                _produtoApp.Update(produtoDomain);
+
+                return RedirectToAction("Index");
+            }
+
+            return View(produto);
+        }
+
+        public ActionResult Deletar(int id)
+        {
+            ViewBag.Message = "Deletar Produto";
+
+            var produto = _produtoApp.GetById(id);
+            var produtoViewModel = Mapper.Map<Produto, ProdutoViewModel>(produto);
+
+            return View(produtoViewModel);
+        }
+
+        [HttpPost, ActionName("Deletar")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeletarConfirmado(int id)
+        {
+            var produto = _produtoApp.GetById(id);
+            _produtoApp.Remove(produto);
+
+            return RedirectToAction("Index");
         }
     }
 }
